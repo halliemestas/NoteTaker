e643dc91421f05cb20a94e53f65e433d9901c385
Hallie <halliemestas@gmail.com>
2022-10-03 15:19:22 -0600

    new note functionality

--- Develop/server.js
+++ Develop/server.js
@@ -23,6 +23,60 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// POST request to add a new note
+app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note`);
+  
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+        // Variable for the object we will save
+        const newNote = {
+        title,
+        text
+        };
+
+        // Obtain existing notes
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+            console.error(err);
+            } else {
+            // Convert string into JSON object
+            const parsedNotes = JSON.parse(data);
+
+            // Add a new note
+            parsedNotes.push(newNote);
+
+            // Write updated notes back to the file
+            fs.writeFile(
+                './db/db.json',
+                JSON.stringify(parsedNotes, null, 4),
+                (writeErr) =>
+                writeErr
+                    ? console.error(writeErr)
+                    : console.info('Successfully updated reviews!')
+            );
+            }
+        })
+  
+        const response = {
+        status: 'success',
+        body: newNote,
+        };
+  
+        console.log(response);
+        res.status(201).json(response);
+    } 
+    else {
+      res.status(500).json('Error in making new note!');
+    }
+  });
+
+;
+
 // Local hosting for testing
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
--- README.md
+++ README.md
@@ -11,6 +11,7 @@ GIVEN a note-taking application
 
 WHEN I open the Note Taker
 THEN I am presented with a landing page with a link to a notes page
+<!-- Done -->
 
 WHEN I click on the link to the notes page
 THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
